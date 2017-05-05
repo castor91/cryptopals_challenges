112bb475bb820d0501ca4cb77beda25b45ab09c1
castor91 <ivan.paterno@studenti.unimi.it>
2017-05-05 15:49:45 +0200

    Added challenge5

--- set1/challenge5.py
+++ set1/challenge5.py
@@ -2,8 +2,15 @@
 import sys
 
 def encr(msg, key="ICE"):
+    msg = [ord(x) for x in msg]
+    result = ''
+    for i in xrange(len(msg)):
+        result += '{:02x}'.format(msg[i] ^ ord(key[i % len(key)]))
+    return result
+
 
 if __name__ == '__main__':
-
-    print encr(sys.argv[1])
+    msg = '''Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal'''
+    print encr(msg)
 
